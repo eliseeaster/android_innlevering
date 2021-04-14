e0ac1791d8d41c2490e843e335fd6cdd65e3cc1f
Amina Brenneng <aminahb@live.no>
2021-04-14 11:38:20 +0200

    added a scrollable, infinite list. Ready to fetch API

---
+++ app/src/main/java/no/kristiania/android_innlevering/ExampleAdapter.kt
@@ -0,0 +1,41 @@
+package no.kristiania.android_innlevering
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+//ViewHolder representerer en enkelt rad i listen. Den refererer til ImageView og TextViews ved hjelp av ID.
+
+class ExampleAdapter(private val exampleList: List<ExampleItem>) : RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExampleViewHolder {
+
+        //.inflate er metoden som gjør layout-filen til et view-object
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.example_item,
+        parent, false)
+
+        return ExampleViewHolder(itemView)
+    }
+//Denne funksjonen blir kalt igjen og igjen mens man scroller eller et item blir oppdatert med ny data.
+    override fun onBindViewHolder(holder: ExampleViewHolder, position: Int) {
+        val currentItem = exampleList[position]
+
+    holder.imageView.setImageResource((currentItem.imageResource))
+    holder.textView1.text = currentItem.text1
+    holder.textView2.text = currentItem.text2
+
+    }
+
+    //Hvor mange items det skal være i listen vår. Det skal være like mange som vi har i exampleList.
+    override fun getItemCount() = exampleList.size
+
+    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.image_view)
+        val textView1: TextView = itemView.findViewById(R.id.text_view_1)
+        val textView2: TextView = itemView.findViewById(R.id.text_view_2)
+    }
+}
--- app/src/main/java/no/kristiania/android_innlevering/HomeActivity.kt
+++ app/src/main/java/no/kristiania/android_innlevering/HomeActivity.kt
@@ -2,6 +2,8 @@ package no.kristiania.android_innlevering
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_home.*
 
 
 class HomeActivity : AppCompatActivity() {
@@ -9,8 +11,27 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val exampleList = generateDummyList(500)
 
+        //brukt plugin under gradle module filen for å få dette til å funke
+        recycler_view.adapter = ExampleAdapter(exampleList)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.setHasFixedSize(true)
 
     }
 
+    private fun generateDummyList(size: Int): List<ExampleItem> {
+
+        val list = ArrayList<ExampleItem>()
+        for (i in 0 until size) {
+            val drawable = when (i % 3) {
+                0 -> R.drawable.ic_android
+                1 -> R.drawable.mane
+                else -> R.drawable.star
+            }
+            val item = ExampleItem(drawable, "Item $i", "Line 2")
+            list += item
+        }
+        return list
+    }
 }
