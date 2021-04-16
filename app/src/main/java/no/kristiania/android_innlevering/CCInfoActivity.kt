250e37a2547e7dd44bd53842e021560abc9765a6
Amina Brenneng <aminahb@live.no>
2021-04-16 13:16:54 +0200

    Made some changes in Manifest, added all pages

--- app/src/main/java/no/kristiania/android_innlevering/CCInfoActivity.kt
+++ app/src/main/java/no/kristiania/android_innlevering/CCInfoActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 //PAGE 4 - CRYPTO CURRENCY INFORMATION
 /*
 - Screen shows a header containing the icon, name, symbol, recent rate of the cryptocurrency whose row was clicked on screen 2.
-- Screen shows how much this cryptocurrency user owns and its value in dollors.
+- Screen shows how much this cryptocurrency user owns and its value in dollars.
 - Users can sell or buy this currency. Sell button should only be enabled if the user owns this cryptocurrency. Buy button should only be enabled if the user's dollar account is not empty.
 Hint button.isEnabled = false | true
 - Graph (BONUS TASK): Screen also shows a graph indicating fluctuations in the value of cryprocurrency over an interval
