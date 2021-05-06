340953fb6cf6c0de9734ccf596811920913bc6a0
Amina Brenneng <aminahb@live.no>
2021-05-06 12:52:28 +0200

    app runs, bug fixed in database version

--- app/src/main/java/no/kristiania/android_innlevering/data/PortfolioDatabase.kt
+++ app/src/main/java/no/kristiania/android_innlevering/data/PortfolioDatabase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
 
-@Database(entities = [Portfolio::class, Transactions::class], version = 1, exportSchema = false)
+@Database(entities = [Portfolio::class, Transactions::class], version = 2, exportSchema = false)
 abstract class PortfolioDatabase : RoomDatabase() {
 
     abstract fun PortfolioDao(): PortfolioDao
